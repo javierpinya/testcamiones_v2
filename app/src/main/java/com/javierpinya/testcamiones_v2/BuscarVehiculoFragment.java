e690520908632f5445f407fb7e46b072e22a44ab
Javier <javierpinya@protonmail.com>
2019-11-12 14:06:28 +0100

    comprobar que se leen los datos del csv y se crea la tabla taccami. También estoy en mitad del BuscarVehiculoFragment...

--- app/src/main/java/com/javierpinya/testcamiones_v2/BuscarVehiculoFragment.java
+++ app/src/main/java/com/javierpinya/testcamiones_v2/BuscarVehiculoFragment.java
@@ -11,6 +11,13 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.javierpinya.testcamiones_v2.Clases.TaccamiEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -22,6 +29,12 @@ public class BuscarVehiculoFragment extends Fragment {
     private Button buscar;
     private String primer="";
     private String segundo = "";
+    private TacprcoViewModel tacprcoViewModel;
+    private TacsecoViewModel tacsecoViewModel;
+    private TaccamiViewModel taccamiViewModel;
+    private TaccatrViewModel taccatrViewModel;
+    private TaccondViewModel taccondViewModel;
+    private TplcprtViewModel tplcprtViewModel;
 
 
     public BuscarVehiculoFragment() {
@@ -42,6 +55,7 @@ public class BuscarVehiculoFragment extends Fragment {
         primerComp = view.findViewById(R.id.etPrimerComp);
         segundoComp = view.findViewById(R.id.etSegundoComp);
         buscar = view.findViewById(R.id.btnBuscarVehiculo);
+        lanzarViewModel();
 
         buscar.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -52,11 +66,68 @@ public class BuscarVehiculoFragment extends Fragment {
                     Toast.makeText(getActivity(), "Introduzca una matrícula", Toast.LENGTH_SHORT).show();
                 }else{
                     //Buscar con el modelView
+                    buscarComponentes(primer, segundo);
                 }
             }
         });
 
         return view;
+    }
+
+    private void buscarComponentes(String primer, String segundo) {
+        final List<Integer> cod_vehiculo1 = new ArrayList<>();
+        final List<Integer> cod_vehiculo2 = new ArrayList<>();
+        final List<Integer> listaEquivalente = new ArrayList<>();
+
+        if (primer.length()>0) {
+            taccamiViewModel.findTaccamiByTMatricula(primer).observe(getActivity(), new Observer<List<TaccamiEntity>>() {
+                @Override
+                public void onChanged(List<TaccamiEntity> taccamiEntities) {
+                    for (int i = 0; i < taccamiEntities.size(); i++) {
+                        cod_vehiculo1.add(taccamiEntities.get(i).getCod_vehiculo());
+                    }
+                }
+            });
+        }
+        if(segundo.length()>0){
+            taccamiViewModel.findTaccamiByCMatricula(segundo).observe(getActivity(), new Observer<List<TaccamiEntity>>() {
+                @Override
+                public void onChanged(List<TaccamiEntity> taccamiEntities) {
+                    for(int i=0;i<taccamiEntities.size();i++) {
+                        cod_vehiculo2.add(taccamiEntities.get(i).getCod_vehiculo());
+                    }
+                }
+            });
+        }
+
+        if(cod_vehiculo1.size()>0 && cod_vehiculo2.size()>0){
+            for(int i=0;i<cod_vehiculo1.size();i++){
+                for(int j=0;j<cod_vehiculo2.size();j++){
+                    if (cod_vehiculo1.get(i) == cod_vehiculo2.get(j)){
+                        listaEquivalente.add(cod_vehiculo1.get(i));
+                    }
+                }
+            }
+        }else{
+            for(int i=0;i<cod_vehiculo1.size();i++){
+                listaEquivalente.add(cod_vehiculo1.get(i));
+            }
+            for(int i=0;i<cod_vehiculo2.size();i++){
+                listaEquivalente.add(cod_vehiculo2.get(i));
+            }
+        }
+
+
+
+    }
+
+    private void lanzarViewModel() {
+        tacprcoViewModel = ViewModelProviders.of(getActivity()).get(TacprcoViewModel.class);
+        tacsecoViewModel = ViewModelProviders.of(getActivity()).get(TacsecoViewModel.class);
+        taccamiViewModel = ViewModelProviders.of(getActivity()).get(TaccamiViewModel.class);
+        taccatrViewModel = ViewModelProviders.of(getActivity()).get(TaccatrViewModel.class);
+        taccondViewModel = ViewModelProviders.of(getActivity()).get(TaccondViewModel.class);
+        tplcprtViewModel = ViewModelProviders.of(getActivity()).get(TplcprtViewModel.class);
     }
 
 }
